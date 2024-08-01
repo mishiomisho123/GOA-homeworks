8848f33c9eef9ce5c36625f5524c327c5b3fe78c
mishiomisho123 <beridzemikheili8@gmail.com>
2024-08-01 23:40:46 +0400

    second commit

---
+++ Level 9/homework/homwork.py
@@ -0,0 +1,33 @@
+print("hello world")
+
+name = ("misho")
+
+lastname = ("beridze")
+
+age = (14)
+
+country = ("georgia")
+
+town = ("batumi")
+
+favorite, color = ("orange")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
